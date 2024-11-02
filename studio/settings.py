090f14109698251a73b656e56a0e54e3d70347fe
Youssef90zip <132919524+Youssef90zip@users.noreply.github.com>
2024-11-02 13:09:36 +0100

    Update settings.py

--- studio/settings.py
+++ studio/settings.py
@@ -142,8 +142,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-#EMAIL_HOST_USER = 'youssefaferdou90@gmail.com'
-#EMAIL_HOST_PASSWORD = 'mkpxdhhyvrtzwbpa'
 
 EMAIL_HOST_USER = 'luproject97@gmail.com'
 EMAIL_HOST_PASSWORD = '.....'
